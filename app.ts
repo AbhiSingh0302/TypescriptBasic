53f6b13de3876c5e8f74f2caaa447d8375b74167
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-06-02 10:51:32 +0530

    ts config file installed and strict mode activated in it

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 const num1El = document.getElementById("num1") as HTMLInputElement; //we have added this because it does not identify what is the type of element
 const num2El = document.getElementById("num2") as HTMLInputElement;
-const buttonEl = document.querySelector("button"); // we dont needed to add the above extra syntax because here it is stated that it is a button element
-
-function add(num1: number, num2: number){
+const buttonEl = document.querySelector("button")!; // we dont needed to add the above extra syntax because here it is stated that it is a button element
+// ! --> to check for null --> required because of strict mode in tsconfig.json file 
+function add(num1: number, num2: number){ // strict mode in tsconfig file forces us to add type on every variable
     return num1 + num2;
 }
 
-buttonEl?.addEventListener('click',() => {
+buttonEl.addEventListener('click',() => {
     const numb1 = num1El.value;
     const numb2 = num2El.value;
     const result = add(+numb1,+numb2);
