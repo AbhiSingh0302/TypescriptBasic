29060253e9a7e252f650b8fe69d172e105ee6efe
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-06-02 12:22:51 +0530

    using object and array types

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-var num1El = document.getElementById("num1"); //we have added this because it does not identify what is the type of element
-var num2El = document.getElementById("num2");
-var buttonEl = document.querySelector("button"); // we dont needed to add the above extra syntax because here it is stated that it is a button element
+"use strict";
+const num1El = document.getElementById("num1");
+const num2El = document.getElementById("num2");
+const buttonEl = document.querySelector("button");
+const numResults = [];
+const stringResult = [];
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2;
+    }
+    return +num1 + +num2;
 }
-buttonEl === null || buttonEl === void 0 ? void 0 : buttonEl.addEventListener('click', function () {
-    var numb1 = num1El.value;
-    var numb2 = num2El.value;
-    var result = add(+numb1, +numb2);
+function printResult(resultObj) {
+    console.log(resultObj.val, " and ", resultObj.timestamp);
+}
+buttonEl.addEventListener('click', () => {
+    const numb1 = num1El.value;
+    const numb2 = num2El.value;
+    const result = add(+numb1, +numb2);
+    const resultStr = add(numb1, numb2);
+    numResults.push(result);
+    stringResult.push(resultStr);
     console.log(result);
+    console.log(resultStr);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, stringResult);
 });
--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 const num1El = document.getElementById("num1") as HTMLInputElement;
 const num2El = document.getElementById("num2") as HTMLInputElement;
 const buttonEl = document.querySelector("button")!; 
+
+const numResults: number[] = [];
+const stringResult: string[] = [];
+
 function add(num1: number | string, num2: number | string){  // union types allowed us to accept more than one type of types
     if(typeof num1 === "number" && typeof num2 === "number"){
         return num1 + num2;
@@ -9,12 +13,19 @@ function add(num1: number | string, num2: number | string){  // union types allo
     }
     return +num1 + +num2;
 }
+function printResult(resultObj: {val: number, timestamp: Date}){
+    console.log(resultObj.val," and ",resultObj.timestamp);
+}
 
 buttonEl.addEventListener('click',() => {
     const numb1 = num1El.value;
     const numb2 = num2El.value;
-    const result = add(+numb1,+numb2);
-    const resultStr = add(numb1,numb2);
+    const result = add(+numb1,+numb2) as number;
+    const resultStr = add(numb1,numb2)as string;
+    numResults.push(result);
+    stringResult.push(resultStr);
     console.log(result);
     console.log(resultStr);
+    printResult({val: result, timestamp: new Date()})
+    console.log(numResults,stringResult);
 })
