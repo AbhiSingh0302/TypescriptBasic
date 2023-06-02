adfdfc9b0f7048c32484281cbefc152ee63b7966
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-06-02 10:41:03 +0530

    js features in ts

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+var num1El = document.getElementById("num1"); //we have added this because it does not identify what is the type of element
+var num2El = document.getElementById("num2");
+var buttonEl = document.querySelector("button"); // we dont needed to add the above extra syntax because here it is stated that it is a button element
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 6));
-console.log(add('1', '6'));
+buttonEl === null || buttonEl === void 0 ? void 0 : buttonEl.addEventListener('click', function () {
+    var numb1 = num1El.value;
+    var numb2 = num2El.value;
+    var result = add(+numb1, +numb2);
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
-function add(num1: number,num2: number){
-    return num1+num2;
+const num1El = document.getElementById("num1") as HTMLInputElement; //we have added this because it does not identify what is the type of element
+const num2El = document.getElementById("num2") as HTMLInputElement;
+const buttonEl = document.querySelector("button"); // we dont needed to add the above extra syntax because here it is stated that it is a button element
+
+function add(num1: number, num2: number){
+    return num1 + num2;
 }
 
-console.log(add(1,6));
-console.log(add('1','6'));
+buttonEl?.addEventListener('click',() => {
+    const numb1 = num1El.value;
+    const numb2 = num2El.value;
+    const result = add(+numb1,+numb2);
+    console.log(result);
+})
